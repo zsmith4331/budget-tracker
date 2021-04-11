b338867c05fb0ecf55dc262c8403dfe1351efae0
Zach Smith <zsmith4331@gmail.com>
2021-04-11 16:13:32 -0400

    updated server.js mongoose connection & added notes

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
+// Dependencies //
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// Set the port application //
+const PORT = process.env.PORT || 8080
 
+// Create express app instance //
 const app = express();
 
+// Logger middleware for Express //
 app.use(logger("dev"));
 
+// Parse application body as JSON //
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve static content for the app //
 app.use(express.static("public"));
 
+// Connecting to MongoDb //
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
-// routes
+// Requiried Routes //
 app.use(require("./routes/api.js"));
 
+// Start server to listen to client requests //
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
